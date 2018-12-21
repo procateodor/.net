f699dc13c34af90440ab9e1db31488c3409a702d
unknown <ghergheluca_edi@yahoo.com>
2018-12-21 05:12:47 +0200

    added Domain page

--- app/ClientApp/src/App.js
+++ app/ClientApp/src/App.js
@@ -3,6 +3,7 @@ import { Route } from 'react-router';
 import { Home } from './components/Home';
 import { FetchData } from './components/FetchData';
 import { Counter } from './components/Counter';
+import { DomainPage } from './components/DomainPage';
 
 export default class App extends Component {
   displayName = App.name
@@ -13,6 +14,7 @@ export default class App extends Component {
         <Route exact path='/' component={Home} />
         <Route path='/counter' component={Counter} />
         <Route path='/fetchdata' component={FetchData} />
+        <Route path='/DomainPage' component={DomainPage} />
       </div>
     );
   }
---
+++ app/ClientApp/src/components/DomainPage.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { Button, Card, CardBody, CardImage, CardTitle, CardText, Col } from 'mdbreact';
+
+export class DomainPage extends Component {
+  render() {
+    return (
+        <div>
+
+        </div>
+    );
+  }
+}
