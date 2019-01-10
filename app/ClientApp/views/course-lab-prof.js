d587a0f2489e3df9886cfe73f1840b9494af0b70
unknown <elena.vizitiu@info.uaic.ro>
2019-01-10 20:56:48 +0200

    fix-course

--- app/ClientApp/views/course-lab-prof.js
+++ app/ClientApp/views/course-lab-prof.js
@@ -1,24 +1,52 @@
 $(document).ready(function () {
-    var title=document.getElementById('input-title');
-    var submit=document.getElementsByClassName('submit')[0];
-    var description=document.getElementsByClassName('description-course')[0];
-    var file=document.getElementById('file').files;
-    submit.onclick = event => {
-        var name=document.createElement('p');
-        name.classList='name';
-        name.innerText=title.value;
-        document.getElementsByClassName('curs1')[0].appendChild(name);
+    var titleCourse=document.getElementById('input-titleCourse');
+    var submitCourse=document.getElementsByClassName('submitCourse')[0];
+    var descriptionCourse=document.getElementsByClassName('descriptionCourse')[0];
+    var fileCourse=document.getElementById('fileCourse').files;
+
+    var titleLab=document.getElementById('input-titleLab');
+    var submitLab=document.getElementsByClassName('submitLab')[0];
+    var descriptionLab=document.getElementsByClassName('descriptionLab')[0];
+    var fileLab=document.getElementById('fileLab').files;
+
+    submitCourse.onclick = event => {
+        var nameCourse=document.createElement('p');
+        nameCourse.classList='nameCourse';
+        nameCourse.innerText=titleCourse.value;
+        document.getElementsByClassName('curs')[0].appendChild(nameCourse);
 
         var courseDescription=document.createElement('p');
         courseDescription.classList='description';
-        courseDescription.innerText=description.value;
-        document.getElementsByClassName('curs1')[0].appendChild(courseDescription);
+        courseDescription.innerText=descriptionCourse.value;
+        document.getElementsByClassName('curs')[0].appendChild(courseDescription);
 
         var a=document.createElement('a');
-        a.files=file;
+        a.files=fileCourse;
         a.href = "";
         a.classList='pdf'
-        a.innerText="Download pdf";
-        document.getElementsByClassName('curs1')[0].appendChild(a);
+        if(a.files!=undefined)
+            a.innerText="Download pdf";
+        console.log(a.files);
+        document.getElementsByClassName('curs')[0].appendChild(a);
+    }
+        submitLab.onclick = event => {
+            var nameLab=document.createElement('p');
+            nameLab.classList='nameLab';
+            nameLab.innerText=titleLab.value;
+            document.getElementsByClassName('lab')[0].appendChild(nameLab);
+    
+            var labDescription=document.createElement('p');
+            labDescription.classList='description';
+            labDescription.innerText=descriptionLab.value;
+            document.getElementsByClassName('lab')[0].appendChild(labDescription);
+    
+            var a=document.createElement('a');
+            a.files=fileLab;
+            a.href = "";
+            a.classList='pdf'
+            if(a.files!=undefined)
+                a.innerText="Download pdf";
+            console.log(a.files);
+            document.getElementsByClassName('lab')[0].appendChild(a);
         }
 });
